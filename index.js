6952fc8de3cfa7daf8891b906de2d6ecefb8598f
sisu-01 <tesv010615@naver.com>
2026-07-16 18:57:08 +0900

    종료 메시지

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ import { checking } from "./checking/checking.js";
 import { booking } from "./booking/booking.js";
 import { login } from "./login/login.js";
 
+// 종료 이벤트 등록
+process.on("SIGINT", async () => {
+  await send_message("🔴 프로그램 종료 (Ctrl+C)");
+  process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+  await send_message("🔴 프로그램 종료 (SIGTERM)");
+  process.exit(0);
+});
+
+process.on("uncaughtException", async (err) => {
+  await send_message(`❌ 치명적인 오류\n${err.stack}`);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", async (reason) => {
+  await send_message(`❌ Promise 오류\n${reason}`);
+  process.exit(1);
+});
+
 // 브라우저 생성
 const browser = await chromium.launch({ headless: false });
 const context = await browser.newContext({
