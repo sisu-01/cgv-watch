49f79216940bb8a9d2fdddc8ef1a04c33091a4e8
sisu-01 <tesv010615@naver.com>
2026-09-10 12:19:19 +0900

    간격 2초로

--- checking/checking.js
+++ checking/checking.js
@@ -77,7 +77,7 @@ export async function checking(isDev = false) {
         logger.info(`${MOVIE_TITLE} ${SCREEN_YMD} 감시 정상 동작 중`);
         lastHeartbeatDate = today;
       }
-      await new Promise(resolve => setTimeout(resolve, 5000));
+      await new Promise(resolve => setTimeout(resolve, 2000));
     } catch (error) {
       send_message('checking.js\n', error);
       logger.error(error);
